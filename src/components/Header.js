15d36c68787777453d5328672e7923eaa8971d5d
SachinMS <sachin01258@gmail.com>
2022-02-18 12:31:19 +0530

    Added Timer and button to start and submit test.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
+import "../styles/header.css";
 
-function Header() {
+const Header = () => {
+  const [minutes, setMinutes] = useState(10);
+  const [seconds, setSeconds] = useState(50);
+  useEffect(() => {
+    let myInterval = setInterval(() => {
+      if (seconds > 0) {
+        setSeconds(seconds - 1);
+      }
+      if (seconds === 0) {
+        if (minutes === 0) {
+          clearInterval(myInterval);
+        } else {
+          setMinutes(minutes - 1);
+          setSeconds(59);
+        }
+      }
+    }, 1000);
+    return () => {
+      clearInterval(myInterval);
+    };
+  });
+
   return (
-    <div>
+    <header>
       <h1>Quiz App</h1>
-    </div>
+      {minutes === 0 && seconds === 0 ? null : (
+        <h1>
+          {" "}
+          {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+        </h1>
+      )}
+      <button className="header-button">Start</button>
+    </header>
   );
-}
+};
 
 export default Header;
