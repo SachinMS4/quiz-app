483a598f631770dde514fcdb27a8addd1fe625f3
SachinMS <sachin01258@gmail.com>
2022-02-18 12:35:48 +0530

    Display total time and questions from data file

--- src/components/QuizStart.js
+++ src/components/QuizStart.js
@@ -2,13 +2,19 @@ import React from "react";
 
 import "../styles/QuizStart.css";
 
+import { useSelector } from "react-redux";
+
 function QuizStart({ setStartQuiz, setDisQue }) {
+  const data = useSelector((state) => state.quiz.value);
+  const totalTime = data.reduce((prev, item) => prev + item.time, 0);
+  console.log(totalTime);
+
   return (
     <div className="start-container">
       <h2>Your are about to start Quiz</h2>
       <ul>
-        <li>Number of questions 10</li>
-        <li>Total time 20</li>
+        <li>Number of questions {data.length}</li>
+        <li>Total time {totalTime} minutes</li>
       </ul>
       <button
         onClick={() => {
