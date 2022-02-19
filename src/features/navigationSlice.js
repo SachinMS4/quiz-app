de5e0d40734a239391a78e1224bb44531341a5bc
SachinMS <sachin01258@gmail.com>
2022-02-19 11:19:56 +0530

    Added navigation state to slice

--- src/features/navigationSlice.js
+++ src/features/navigationSlice.js
@@ -1,17 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { data } from "../assets/data";
 
-const totalTime = data.reduce((prev, item) => prev + item.time, 0);
 const initialState = {
-  value: [],
+  value: { startPage: true, endPage: false, quesPage: false },
 };
 
 const navigationSlice = createSlice({
   name: "nav",
   initialState,
-  reducers: {},
+  reducers: {
+    changeNav: (state, { payload }) => {
+      state.value = payload;
+    },
+  },
 });
 
-export const {} = navigationSlice.actions;
+export const { changeNav } = navigationSlice.actions;
 
 export default navigationSlice.reducer;
