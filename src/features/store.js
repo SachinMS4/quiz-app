1485f8de0c8655cca856f060f0763a0d4c47c14a
SachinMS <sachin01258@gmail.com>
2022-02-19 11:24:29 +0530

    Added navigation reducer

--- src/features/store.js
+++ src/features/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import questionReducer from "./quizSlice";
+import navigationReducer from "./navigationSlice";
 
 export const store = configureStore({
   reducer: {
     quiz: questionReducer,
+    nav: navigationReducer,
   },
 });
